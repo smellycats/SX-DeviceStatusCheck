03e5dd0a0be31659b4f81458fd5e44b06dd70f39
SmellyCat <smellycat2014@foxmail.com>
2017-03-17 17:02:13 +0800

    更新 测试方法

--- request_test.py
+++ request_test.py
@@ -8,6 +8,7 @@ import arrow
 #from requests.auth import HTTPBasicAuth, HTTPDigestAuth
 
 from helper_device import Device
+from helper_ping import *
 from ini_conf import MyIni
 
 class DeviceTest(object):
@@ -18,23 +19,33 @@ class DeviceTest(object):
     
     def test_get_device(self):
         """上传卡口数据"""
-        r = self.dev.get_device_list()
-        print r
-        r2 = self.dev.get_device_by_ip('127.0.0.1')
-        print r2
-        r3 = self.dev.get_device_check()
-        print r3
+        #print self.dev.get_device_list()
+        #print self.dev.get_device_list(type=2)
+        #print self.dev.get_device_list(type=3)
+        print self.dev.get_device_by_ip('127.0.0.1')
+        print self.dev.get_device_check(num=20, type=2)
+        print self.dev.get_device_check(num=10, type=3)
         #assert r['headers'] == 201
 
     def test_set_device(self):
         data = [
-                    {'ip': '127.0.0.1', 'status': True},
-                    {'ip': '127.0.0.2', 'status': False}
-                ]
+            {'ip': '127.0.0.1', 'status': True},
+            {'ip': '127.0.0.2', 'status': False}
+        ]
         r = self.dev.set_device(data)
         print r
+
+class PingTest(object):
+    def __init__(self):
+        pass
+
+    def test_ping(self):
+        ping('192.168.1.123')
+
 
 if __name__ == '__main__':  # pragma nocover
     dt = DeviceTest()
     dt.test_get_device()
-    dt.test_set_device()
+    #dt.test_set_device()
+    #pt = PingTest()
+    #pt.test_ping()
