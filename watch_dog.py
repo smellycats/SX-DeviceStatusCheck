d67d757b27fbb5c0c248ee50c1de49a5a253a4e5
SmellyCat <smellycat2014@foxmail.com>
2017-03-20 14:18:55 +0800

    更新 添加id作为devce更新的查询条件

--- watch_dog.py
+++ watch_dog.py
@@ -22,6 +22,7 @@ class WatchDog(object):
         self.my_ini = MyIni()
         # 设备状态实例
         self.dev = Device(**self.my_ini.get_device())
+        self.dev.base_path = ''
         # 进程池
         self.pool = mul.Pool(8)
         # 循环检测次数
@@ -40,29 +41,32 @@ class WatchDog(object):
         rel = self.pool.map(ping, ip_list)
         for i, j in zip(dev_list, rel):
             if j is True:
-                dev_true_list.append({'ip': i['ip'], 'status': True})
+                dev_true_list.append(
+                    {'id': i['id'], 'ip': i['ip'], 'status': True})
                 continue
             if j is False and i['status'] is False:
-                dev_true_list.append({'ip': i['ip'], 'status': False})
+                dev_true_list.append(
+                    {'id': i['id'], 'ip': i['ip'], 'status': False})
                 continue
-            dev_false_list.append({'ip': i['ip'], 'status': False})
+            dev_false_list.append(
+                {'id': i['id'], 'ip': i['ip'], 'status': False})
 
         return dev_true_list, dev_false_list
                 
 
     def set_device_status(self, dev_list):
+        """更新设备状态"""
         if len(dev_list) > 0:
             self.dev.set_device(dev_list)
             for i in dev_list:
                 print '{0} {1} {2}'.format(
                     str(arrow.now()), i['ip'], i['status'])
-                logger.info('{0} {1}'.format(i['ip'], i['status']))
+                #logger.info('{0} {1}'.format(i['ip'], i['status']))
     
-    def device_status_check(self):
+    def device_status_check(self, type):
         """"设备状态检测"""
-        n = random.randint(2, 3)
         dev_true_list = []
-        dev_false_list = self.dev.get_device_check(num=24, type=n)['items']
+        dev_false_list = self.dev.get_device_check(num=24, type=type)['items']
         if len(dev_false_list) == 0:
             return
         for i in range(self.loop):
@@ -75,7 +79,8 @@ class WatchDog(object):
     def run(self):
         while 1:
             try:
-                self.device_status_check()
+                for i in [2, 3]:
+                    self.device_status_check(i)
                 time.sleep(1)
             except Exception as e:
                 print e
